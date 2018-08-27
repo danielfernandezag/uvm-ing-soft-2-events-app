5b8a2f3710c4b979b5ed4d9a3fcc72ed27a31a89
Julio Daniel Fernandez Aguirre <danielkiske@live.com.mx>
2018-08-26 20:52:36 -0500

    componentes basicos y dependencias

---
+++ src/Componentes/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
