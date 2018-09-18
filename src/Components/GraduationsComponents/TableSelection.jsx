30d84ae087114a2a3e29054f948ea86ae8f69192
daniel fernandez aguirre <danielfernandezaguirre@outlook.com>
2018-09-18 11:02:12 -0500

    seccion de seleccion de mesas y correccion en login

--- src/Components/GraduationsComponents/TableSelection.jsx
+++ src/Components/GraduationsComponents/TableSelection.jsx
@@ -1,10 +1,91 @@
 import React, { Component } from 'react'
-import { Jumbotron } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron, Button } from 'react-bootstrap';
 export default class TableSelection extends Component {
   render() {
     return (
-      <Jumbotron>
-        Tables
+      <Jumbotron id="TableSelectionContainer">
+        <Grid>
+          <Row>
+            <Col xs={6} xsOffset={3} md={6} mdOffset={3} lg={6} lgOffset={3}>
+              <div className="stage">ESCENARIO</div>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={1} xsOffset={2} md={1} mdOffset={2} lg={1} lgOffset={2}>
+              <Button className="tableButton">MESA1</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA2</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA3</Button>
+            </Col>
+            <Col xs={2} md={2} lg={2}>
+              <div className="dancefloor">PISTA DE BAILE</div>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA4</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA5</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA6</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={1} xsOffset={2} md={1} mdOffset={2} lg={1} lgOffset={2}>
+              <Button className="tableButton">MESA7</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA8</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA9</Button>
+            </Col>
+            <Col xs={2} md={2} lg={2}>
+              <div className="dancefloor2"></div>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA10</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA11</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA12</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={1} xsOffset={2} md={1} mdOffset={2} lg={1} lgOffset={2}>
+              <Button className="tableButton">MESA13</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA14</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA15</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA16</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA17</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA18</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA19</Button>
+            </Col>
+            <Col xs={1} md={1} lg={1}>
+              <Button className="tableButton">MESA20</Button>
+            </Col>
+          </Row>
+        </Grid>
       </Jumbotron>
     )
   }
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,7 +3,7 @@ import { Grid, Row, Col, FormControl, FormGroup, ControlLabel, Button } from 're
 import ModalBlocked from './ModalBlocked';
 
 export default class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       users: this.props.users,
@@ -12,41 +12,42 @@ export default class Login extends Component {
       loginAttempts: 0,
       loginError: false,
       loginBlocked: false,
-
     }
   }
-  componentWillMount(){
-    localStorage.getItem("blocked") && this.setState({ loginBlocked: true }); 
+  componentWillMount() {
+    const _blockEndDate = localStorage.getItem("blocked");
+    console.log("bloqueado");
+    if (_blockEndDate) { this.setState({ loginBlocked: true }) }
   }
-
-  componentDidMount(){
+  
+  componentDidMount() {
     console.log(this.state.users);
   }
 
-  submitLogin = (e) => { 
-    e.preventDefault(); 
-    const userIndex = this.state.users.findIndex((user)=> user.account === this.state.account);
+  submitLogin = (e) => {
+    e.preventDefault();
+    const userIndex = this.state.users.findIndex((user) => user.account === this.state.account);
     console.log(userIndex);
     const currentLoginData = this.state.users[userIndex];
     console.log(currentLoginData);
-    if( currentLoginData.password === this.state.password ){
+    if (currentLoginData.password === this.state.password) {
       console.log("success " + this.state.password + "|" + currentLoginData.password);
       this.props.login();
     }
-    else{
+    else {
       console.log("success " + this.state.password + "|" + currentLoginData.password);
       let newAttempts = this.state.loginAttempts;
       newAttempts++;
       console.log("attemps " + newAttempts);
       this.setState({ loginAttempts: newAttempts, loginError: true });
-      if(this.state.loginAttempts === 5){
-        localStorage.setItem("blocked","true");
+      if (this.state.loginAttempts === 5) {
+        localStorage.setItem("blocked", "true");
         this.setState({ loginAttempts: 0, loginBlocked: true });
       }
-    } 
+    }
   };
 
-  getValidationStateAccount = () => /^[0-9]{9,10}$/.test(this.state.account) === true ? "success" :  ( this.state.account.length === 0 ? null : "error") ;  
+  getValidationStateAccount = () => /^[0-9]{9,10}$/.test(this.state.account) === true ? "success" : (this.state.account.length === 0 ? null : "error");
   handleChangeAccount = (e) => this.setState({ account: e.target.value });
   handleChangePassword = (e) => this.setState({ password: e.target.value });
 
@@ -55,19 +56,19 @@ export default class Login extends Component {
     return (
       <Grid id="Login">
         <Row>
-          <Col xs={10} xsOffset={1} md={4} mdOffset={4}  lg={4} lgOffset={4}>
-            { this.state.loginBlocked && <ModalBlocked show={this.state.loginBlocked}/> }
+          <Col xs={10} xsOffset={1} md={4} mdOffset={4} lg={4} lgOffset={4}>
+            {this.state.loginBlocked && <ModalBlocked show={this.state.loginBlocked} />}
             <form onSubmit={this.submitLogin}>
               <FormGroup controlId="formAccount" validationState={this.getValidationStateAccount()}>
-                <label>Numero de Cuenta</label>
-                {this.getValidationStateAccount() === "success" ? <ControlLabel>numero de cuenta valido</ControlLabel> : <ControlLabel>numero de cuenta invalido</ControlLabel>}
-                <FormControl type="text" value={this.state.account} placeholder="ingresa tu numero de cuenta" onChange={this.handleChangeAccount} required/>
+                <label> Numero de Cuenta &ensp;</label>
+                {this.getValidationStateAccount() === "success" ? <ControlLabel> numero de cuenta valido </ControlLabel> : <ControlLabel> numero de cuenta invalido </ControlLabel>}
+                <FormControl type="text" value={this.state.account} placeholder="ingresa tu numero de cuenta" onChange={this.handleChangeAccount} required />
                 <FormControl.Feedback />
               </FormGroup>
               <FormGroup controlId="formPassword" validationState={this.state.loginError ? "error" : null}>
-                <label>Contraseña</label>
-                {this.state.loginError && <ControlLabel>error numero de cuenta o contraseña incorrectos</ControlLabel> }
-                <FormControl type="password" value={this.state.password} placeholder="ingresa tu contraseña" onChange={this.handleChangePassword} required/>
+                <label>Contraseña &ensp;</label>
+                {this.state.loginError && <ControlLabel> cuenta o contraseña incorrecta </ControlLabel>}
+                <FormControl type="password" value={this.state.password} placeholder="ingresa tu contraseña" onChange={this.handleChangePassword} required />
                 <FormControl.Feedback />
               </FormGroup>
               <Button type="submit" id="LoginButton" >Acceder</Button>
