c7bf14c76ef2150fcdc933d3a9808e85697764e5
daniel fernandez aguirre <danielfernandezaguirre@outlook.com>
2018-11-12 15:11:03 -0600

    login style

--- src/Components/GraduationsComponents/Carroussel.jsx
+++ src/Components/GraduationsComponents/Carroussel.jsx
@@ -4,18 +4,21 @@ import C1 from '../../Images/Carroussel-1.jpeg';
 import C2 from '../../Images/Carroussel-2.jpeg';
 import C3 from '../../Images/Carroussel-3.jpeg';
 
+
+const width = window.screen.width;
+
 export default class Carroussel extends Component {
   render() {
     return (
       <Carousel>
         <Carousel.Item>
-          <img width={1366} alt="evento-1" height={500} src={C1} />
+          <img width={width} alt="evento-1" height={500} src={C1} />
         </Carousel.Item>
         <Carousel.Item>
-          <img width={1366} alt="evento-2" height={500} src={C2} />
+          <img width={width} alt="evento-2" height={500} src={C2} />
         </Carousel.Item>
         <Carousel.Item>
-          <img width={1366} alt="evento-3" height={500} src={C3} />
+          <img width={width} alt="evento-3" height={500} src={C3} />
         </Carousel.Item>
       </Carousel>
     )
